00b6a00ba767bf2615d226d03dd93ba4a8f9c8c6
Kalina Dukova <kalina984@abv.bg>
2022-08-28 14:51:30 +0300

    added TS to random beer context

---
+++ src/context/randomBeerContext.tsx
@@ -0,0 +1,42 @@
+import { createContext, useState, useMemo, useCallback } from 'react';
+import { Beer } from '../pages/Home';
+
+interface RandomBeerProviderProps {
+  children: React.ReactNode;
+}
+
+interface RandomBeerContextProps {
+  randomBeer: Beer[];
+  randomBeerGenerator: () => void;
+}
+
+const defaultRandomBeer = {
+  randomBeer: [],
+  randomBeerGenerator: () => {},
+};
+
+export const RandomBeerContext =
+  createContext<RandomBeerContextProps>(defaultRandomBeer);
+
+export const RandomBeerProvider: React.FC<RandomBeerProviderProps> = ({
+  children,
+}) => {
+  const [randomBeer, setRandomBeer] = useState<Beer[]>([]);
+
+  function randomBeerGenerator() {
+    setRandomBeer([]);
+    fetch('https://api.punkapi.com/v2/beers/random')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // localStorage.setItem('randomBeer', JSON.stringify(data));
+        setRandomBeer(data);
+      });
+  }
+
+  return (
+    <RandomBeerContext.Provider value={{ randomBeer, randomBeerGenerator }}>
+      {children}
+    </RandomBeerContext.Provider>
+  );
+};
--- src/pages/RandomBeer.tsx
+++ src/pages/RandomBeer.tsx
@@ -5,19 +5,18 @@ import { Beer } from '../pages/Home';
 import { useContext, useEffect } from 'react';
 
 import { RandomBeerContext } from '../context/randomBeerContext';
+import BeerCard from '../components/BeerCard/BeerCard';
 
 const RandomBeer: React.FC = () => {
   const { randomBeer, randomBeerGenerator } = useContext(RandomBeerContext);
 
-  useEffect(() => {
-    randomBeerGenerator();
-  }, []);
+  // useEffect(() => {
+  //   randomBeerGenerator();
+  //   randomBeerTest = randomBeer[0];
+  //   console.log(randomBeer[0]);
+  // }, []);
 
-  return (
-    <>
-      <BeerList beersArray={randomBeer} />
-    </>
-  );
+  return <BeerList beersArray={randomBeer} />;
 };
 
 export default RandomBeer;
